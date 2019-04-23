f95a0617849645e2b6e1a171d7ecc2f7146980d2
David Nuescheler <david@nuescheler.ch>
2019-04-23 13:48:08 -0600

    scrani and .htl cleanup

--- dist/js/scrani.js
+++ dist/js/scrani.js
@@ -2,27 +2,29 @@
     const scrani = (() => {
     
         const scrani = { 
-            animations: [],
+            
+            // config
+            animations: [            
+                {selector: "section", animation:"eager-appear"},
+                {selector: "section img", animation:"wipe"}
+            ],
+
             scrollY: -1,
             scrollYBottom:  0,
         }
 
-        // setup onload
-        scrani.setup = (config) => {
-            for (var i=0; i<config.length; i++) {
-                var conf=config[i];
-                var a={};
-                a.animation=conf.animation;
-                a.selector=conf.selector;
-                a.elems=document.querySelectorAll(conf.selector);
-                scrani.animations.push(a);   
+        // setup
+        scrani.setup = () => {
+            for (let i=0; i<scrani.animations.length; i++) {
+                a = scrani.animations[i];
+                a.elems=document.querySelectorAll(a.selector);
             }
         }
 
         // update single element
         scrani.updateElement = (el, animation) => {
-            var progress=0.0;
-            var offsetTop = el.getBoundingClientRect().top+window.pageYOffset;
+            let progress=0.0;
+            const offsetTop = el.getBoundingClientRect().top+window.pageYOffset;
 
             if (this.scrollY > offsetTop) {
                 progress=1.0;
@@ -37,13 +39,13 @@
             if (progress>1) progress=1;
             
             if (animation == "eager-appear") {
-                var transY=100-progress*100;
-                var opacity=progress;
+                const transY=100-progress*100;
+                const opacity=progress;
                 el.style=`opacity: ${opacity}; transform: translateY(${transY}px)`;    
             }
 
             if (animation == "wipe") {
-                var right=Math.round(100-progress*100);
+                const right=Math.round(100-progress*100);
                 el.style=`clip-path: inset(0 ${right}% 0 0)`;
             }
         }
@@ -56,12 +58,24 @@
             scrani.scrollY = scrollY;
             scrani.scrollYBottom = scrollY+window.innerHeight;
 
-            for (var i=0; i<scrani.animations.length;i++) {
-                var a = scrani.animations[i];
-                for (var j=0; j<a.elems.length;j++) {
+            for (let i=0; i<scrani.animations.length;i++) {
+                const a = scrani.animations[i];
+                for (let j=0; j<a.elems.length;j++) {
                     scrani.updateElement(a.elems[j], a.animation);
                 }
             }
+        }
+
+        //to be called onload
+        scrani.onload = () => {
+            
+            scrani.setup();
+            const repaint = () => {
+                scrani.update(window.scrollY)
+                window.requestAnimationFrame(repaint)
+            }
+            window.requestAnimationFrame(repaint);
+  
         }
 
         return (scrani)
@@ -70,4 +84,3 @@
     window.scrani = scrani; 
 
 })();
-
